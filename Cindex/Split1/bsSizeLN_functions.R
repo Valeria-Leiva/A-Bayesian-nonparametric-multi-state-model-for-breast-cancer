72ef92e99b40eee5ae1ed0766ecff05d968e64a9
Valeria Leiva Yamaguchi <vjleiva@uc.cl>
2026-06-23 12:39:57 -0400

    Add files via upload

---
+++ Cindex/Split1/bsSizeLN_functions.R
@@ -0,0 +1,82 @@
+# Evaluation predictive distributions
+# by transitions - hospital - covars
+
+# rm(list=ls())
+
+
+# set working directory ---------------------------------------------------
+setwd("/home/vjleiva/BNP_Multistate/TrainTest/Split1")
+
+
+# libraries ---------------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse,survival,ggplot2,splines)
+
+library("stats")
+
+ # BNP fit -----------------------------------------------------------------
+
+
+options("max.print"=1000000)
+mem.maxVSize(32000000000)
+
+load("Res_bsSizeLNO_BNP_1_zellnerTrain.RData")
+betah2 <- out.DDP2$BUGSoutput$sims.list$betah
+# dim(betah2) #Nmc p Tr Hdp
+bk2 <- out.DDP2$BUGSoutput$sims.list$bk
+# dim(bk2) #Nmc hosp
+wh2 <- out.DDP2$BUGSoutput$sims.list$BetaProb
+# dim(wh2) #Nmc Hdp Tr
+sig2 <- out.DDP2$BUGSoutput$sims.list$sigma2
+# dim(sig2) #Nmc  Tr
+M2<- out.DDP2$BUGSoutput$sims.list$M
+# dim(M2) #Nmc 1
+nstar <- out.DDP2$BUGSoutput$sims.list$m
+# dim(nstar) #Nmc n Tr
+
+
+Nmc <- dim(betah2)[1]
+Hdp <- dim(betah2)[4]
+tra <- dim(betah2)[3]
+p <- dim(betah2)[2]
+# nper <- dim(nstar)[2]
+
+pred_dist <- function(y,xx,tr,hp){
+  idh = hp
+  Fmcdp <- NULL
+  for(imc in 1:Nmc){
+    Fmcdp[imc]=1-rowSums( do.call('cbind',lapply(1:Hdp,function(jdp) wh2[imc,jdp,tr]*plnorm(y,meanlog=as.numeric(betah2[imc,,tr,jdp]%*%xx)+bk2[imc,idh],
+                                                                                            sdlog = sqrt(sig2[imc,tr])))))
+  }
+  return(Fmcdp)
+}
+
+eval_p=function(y,xx,tr,hp){
+  eval.pred = lapply(1:length(y),function(h) pred_dist(y[h],xx,tr,hp))
+  eval.pred.mean = unlist(lapply(eval.pred,mean))
+  eval.pred.q =  do.call('rbind',lapply(eval.pred,function(h) quantile(h,c(0.025,0.975))))
+  return(cbind(
+    pred_mean=eval.pred.mean,
+    pred_q=eval.pred.q
+  ))
+}
+
+eval_p_mean=function(y,xx,tr,hp){
+  eval.pred = lapply(1:length(y),function(h) pred_dist(y[h],xx,tr,hp))
+  eval.pred.mean = unlist(lapply(eval.pred,mean))
+  # eval.pred.q =  do.call('rbind',lapply(eval.pred,function(h) quantile(h,c(0.025,0.975))))
+  return(cbind(
+    pred_mean=eval.pred.mean
+    # ,pred_q=eval.pred.q
+  ))
+}
+
+eval_p_cindex=function(y.x,xx.x,tr.x,hp.x,
+                       y.y,xx.y,tr.y,hp.y){
+  eval.pred.x = lapply(1:length(y.x),function(h) pred_dist(y.x[h],xx.x,tr.x,hp.x))
+  eval.pred.y = lapply(1:length(y.y),function(h) pred_dist(y.y[h],xx.y,tr.y,hp.y))
+  
+  return(mean(eval.pred.x[[1]]<eval.pred.y[[1]]))
+}
+
+
---
+++ Cindex/Split1/tr1_cindex.R
@@ -0,0 +1,48 @@
+# Cindex - transition 6
+# rm(list = ls())
+
+
+# set directory worj ------------------------------------------------------
+setwd("/home/vjleiva/BNP_Multistate/TrainTest/Split1")
+
+
+# functions ---------------------------------------------------------------
+source("bsSizeLN_functions.R")
+
+
+# data --------------------------------------------------------------------
+load("Cindexinfo.Rdata")
+tr=1
+
+# elements for posterior mean
+y.x = to.cindex.info[[tr]]$time.x
+xx.x = cbind(rep(1,nrow(to.cindex.info[[tr]])),
+             to.cindex.info[[tr]]$age.x,to.cindex.info[[tr]]$ln.x,
+             to.cindex.info[[tr]]$bs1.x,to.cindex.info[[tr]]$bs2.x,to.cindex.info[[tr]]$bs3.x,
+             to.cindex.info[[tr]]$gr2.x,to.cindex.info[[tr]]$gr3.x,
+             to.cindex.info[[tr]]$er.x)
+
+y.y = to.cindex.info[[tr]]$time.y
+xx.y = cbind(rep(1,nrow(to.cindex.info[[tr]])),
+             to.cindex.info[[tr]]$age.y,to.cindex.info[[tr]]$ln.y,
+             to.cindex.info[[tr]]$bs1.y,to.cindex.info[[tr]]$bs2.y,to.cindex.info[[tr]]$bs3.y,
+             to.cindex.info[[tr]]$gr2.y,to.cindex.info[[tr]]$gr3.y,
+             to.cindex.info[[tr]]$er.y)
+
+hp.x=to.cindex.info[[tr]]$hosp.x
+hp.y=to.cindex.info[[tr]]$hosp.y
+
+
+# computation for x and y -------------------------------------------------
+# tr1_f.x=sapply(1:length(y.x),function(h)  {eval_p_mean(y.x[h],xx=xx.x[h,],tr,hp=hp.x[h])}) 
+# tr1_f.y=sapply(1:length(y.y),function(h)  {eval_p_mean(y.y[h],xx=xx.y[h,],tr,hp=hp.y[h])}) 
+
+
+# tr1_cindex = cbind(tr1_f.x,tr1_f.y)
+tr1_cindex = sapply(1:nrow(to.cindex.info[[tr]]),
+                    function(h) {  eval_p_cindex(y.x=y.x[h],xx.x=xx.x[h,],tr.x=tr,hp.x=hp.x[h],
+                                                 y.y=y.y[h],xx.y=xx.y[h,],tr.y=tr,hp.y=hp.y[h]) })
+# file --------------------------------------------------------------------
+save(tr1_cindex,file=paste0("tr",tr,"_cindex.Rdata"))
+
+
